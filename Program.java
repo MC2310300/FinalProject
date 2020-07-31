475add14670e703f198a4efdd5b7884051990e34
MelissaCalvert <69029475+MelissaCalvert@users.noreply.github.com>
2020-07-30 23:05:01 -0700

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+
+package DCApp;
+
+public class Program {
+		
+		protected String type, progress;
+		public Scanner sc = new Scanner(System.in);
+		
+}
